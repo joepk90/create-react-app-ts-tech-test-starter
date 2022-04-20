640e3aa6615b3cf70d6cb264a2b898ca4a6ccefa
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-20 22:24:53 +0100

    install tacit-css package as npm dependancy. include library in index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tacit-css/dist/tacit-css-1.5.5.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
