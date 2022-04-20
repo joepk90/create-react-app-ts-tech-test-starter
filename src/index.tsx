fa2b5ea501b08c50c573c4f36e5b8087d20af2e1
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-20 22:40:31 +0100

    setup base scss. import into the index.js file

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'normalize.css';
+import "src/scss/style.scss"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
