dddfdd74a953c46ac5a0b05bf21b70535754d90e
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-20 22:31:59 +0100

    add basic html in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div className="App">
-
+      <h1>Create React App (TS) Tech Test Starter</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
   );
 }
